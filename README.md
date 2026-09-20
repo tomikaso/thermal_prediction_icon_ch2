f171bccdf9566a58496c029bf178eee7357f0b44
tomikaso <thomas.kamps@sunrise.ch>
2026-09-20 19:49:45 +0200

    Improvements :-)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # thermal_prediction_icon_ch2
+for my local environment ;-) source ~/.venv/bin/activate
--- fetch_icon_ch2.py
+++ fetch_icon_ch2.py
@@ -1,6 +1,25 @@
 import datetime as dt
 import earthkit.data as ekd
 import numpy as np
+import math
+
+def calculate_dew_point(T_celsius, q, P_hpa=1013.25):
+    
+    # 1. convert specific humidity (q) in the mixing ratio (r)
+    r = q / (1.0 - q)
+    
+    # 2. calculate steam pressure (e) in hPa
+    e = (r * P_hpa) / (0.622 + r)
+    
+    # check if e is less than or equal to zero to avoid math domain error
+    if e <= 0:
+        return float('-inf')
+        
+    # 3. calculate dew point (Td) with the Magnus formula
+    alpha = math.log(e / 6.112)
+    Td = (243.12 * alpha) / (17.62 - alpha)
+    
+    return Td
 
 number_of_locations = 14
 locations = ['Scheidegg', 'Locarno', 'Hüsliberg', 'Pany', 'Solothurn', 'Scuol', 'Fiesch', 'Niesen',
@@ -12,6 +31,7 @@ coordinates = np.array([[47.289, 8.915], [46.175384, 8.793927], [47.181896, 9.05
 # define temperature
 temperature = np.full((number_of_locations, 81, 12), 0.00) # 81 levels, 14 locations, 12 time steps
 altitude = np.full((number_of_locations, 81, 12), 0.00) # 81 levels, 14 locations, 12 time steps
+dewpoint = np.full((number_of_locations, 81, 12), 0.00) # 81 levels, 14 locations, 12 time steps
 
 # required data is Temperature, Dewpoint and vertical Levels (HHL)
 forecast = ekd.from_source(
@@ -27,6 +47,21 @@ forecast = ekd.from_source(
 
 dataset = forecast.to_xarray(time_dims=["forecast_reference_time", "step"], squeeze=False)
 print(dataset)
+
+# required data is Temperature, Dewpoint and vertical Levels (HHL)
+forecast_qv = ekd.from_source(
+    "meteoswiss-opendata",
+    collection="ogd-forecasting-icon-ch2",
+    variable="QV",
+    perturbed=False,
+    ref_time="latest",
+    lead_time=[
+        dt.timedelta(hours=1)
+    ],
+)
+
+dataset_qv = forecast_qv.to_xarray(time_dims=["forecast_reference_time", "step"], squeeze=False)
+print(dataset_qv)
 
 # now get the vertical levels from the ICON-CH2 dataset
 vertical_ch2 = ekd.from_source(
@@ -66,10 +101,16 @@ for loc in locations:
         height = ds_hhl_ch2["h"].sel(
                 level=desired_level,
                 values=next_index)
+        specific_humidity = dataset_qv["q"].sel(
+            level=desired_level,
+            values=next_index)
+        dew_point = calculate_dew_point(temperature[locations.index(loc), level, 0], specific_humidity.values.item())
         altitude[locations.index(loc), level, 0] = height.values.item()
         temp_celsius = abs_temp.values.item() - 273.15
-        print(f"Temperatur {loc} Level {desired_level}: {abs_temp.values.item():.2f} K ({temp_celsius:.2f} °C)")
+        print(f"Temperatur {loc} Level {desired_level}, {height.values.item()}: {temp_celsius:.2f} °C, Dew Point: {dew_point:.2f} °C")
+        print(f"Specific. Humidity: {specific_humidity.values.item():.5f}")
         level += 1
-print(f"Temperature Array: {temperature}")
+print(f"saving temperature, altitude and dewpoint to numpy arrays")
 np.save('temperature_icon_ch2.npy', temperature)
 np.save('altitude_icon_ch2.npy', altitude)
+np.save('dewpoint_icon_ch2.npy', dewpoint)
